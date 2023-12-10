56eda545990e407ef95687f235a4c268fdd6d3a4
vadymkos <vadym.kostucok@partnersbanka.cz>
2023-12-10 18:48:07 +0100

    removing styles and body flex, adding pages, styled components, vector borders

---
+++ scooter-landing/src/pages/AppWork.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const VectorTest = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100vh;
+`
+
+export const AppWork = () => {
+    return (
+        <VectorTest>AppWork</VectorTest>
+    )
+}
